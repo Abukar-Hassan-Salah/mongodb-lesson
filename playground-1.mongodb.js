9431e264fe22baa543c289cd3fda368fc0a7e2e7
Abukar-Hassan-Salah <abukarhassansalah@gmail.com>
2023-12-04 13:53:14 +0300

    first commit

---
+++ playground-1.mongodb.js
@@ -0,0 +1,34 @@
+
+// creating  database
+use('university');
+// creating collection
+db.createCollection("Students")
+// Insert a few documents into the sales collection.
+db.getCollection('students').insertMany([
+  {
+    name : "Abukar Hassan",
+    age : 23,
+    major: "engineering",
+    gpa : 90
+  }
+,
+  {
+    name : "isse Hassan",
+    age : 24,
+    major: "computer science",
+    gpa : 92
+  }
+,
+  {
+    name : "hussen Hassan",
+    age : 25,
+    major: "health",
+    gpa : 78
+  }
+  
+]);
+// fetching data in the collection
+
+// updating the data in the collection
+
+
